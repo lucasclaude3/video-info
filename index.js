acd0ca09941dd6205f2b8fdfabcf393e5f52bc2c
kahlua <lucasclaude3@gmail.com>
2019-05-29 17:35:40 +0200

    Send back duration to the renderer process and print it

--- index.js
+++ index.js
@@ -16,6 +16,6 @@ app.on('ready', () => {
 
 ipcMain.on('video:submit', (event, path) => {
   ffmpeg.ffprobe(path, (err, metadata) => {
-    console.log(metadata.format.duration);
+    mainWindow.webContents.send('video:metadata', metadata.format.duration);
   });
 });
